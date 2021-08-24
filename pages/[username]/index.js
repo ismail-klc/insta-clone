ff32f2c19e649966d6bb3cde25107ff89b5072a9
İsmail Kılıç <152120171049@ogrenci.ogu.edu.tr>
2021-08-24 12:27:28 +0300

    profile page updated

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -12,10 +12,10 @@ function Profile() {
         <div className="flex flex-col">
             <Navbar />
             <main className="bg-gray-50 min-h-screen min-w-screen mt-14">
-                <div className="mx-auto sm:max-w-[930px] flex flex-col space-x-5 mt-7">
+                <div className="mx-auto sm:max-w-[930px] flex flex-col mt-7">
                     <ProfileHeader isMe={username === 'myaccount'} />
 
-                    <div className="grid grid-cols-6 gap-7 mb-10">
+                    <div className="grid grid-cols-6 gap-1 md:gap-6 mb-10">
                         {
                             [1, 2, 3, 4, 5, 6, 7].map((a) => (
                                 <ProfilePost key={a}/>
